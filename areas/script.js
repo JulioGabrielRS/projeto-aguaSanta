a670db64d3b66e2096f11f78370eeb994ad7761d
Júlio Gabriel <juliogabriel10@icloud.com>
2025-05-02 08:33:11 -0300

    new

--- areas/script.js
+++ areas/script.js
@@ -1,25 +1,25 @@
 // script.js
 function abrirModal(titulo, descricao, imagem) {
-  document.getElementById('modal-title').textContent = titulo;
-  document.getElementById('modal-text').textContent = descricao;
-  document.getElementById('modal-image').src = imagem;
-  document.getElementById('modal').style.display = 'flex';
+  document.getElementById("modal-title").textContent = titulo;
+  document.getElementById("modal-text").textContent = descricao;
+  document.getElementById("modal-image").src = imagem;
+  document.getElementById("modal").style.display = "flex";
 }
 
 function fecharModal() {
-  document.getElementById('modal').style.display = 'none';
+  document.getElementById("modal").style.display = "none";
 }
 
-window.addEventListener('scroll', function () {
-  const header = document.querySelector('.menu');
-  const logo = document.querySelector('.logo');
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".menu");
+  const logo = document.querySelector(".logo");
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 50) {
-      header.classList.add('reduzido');
-      logo.src = '../images/NovaImagem.png'; // Substitua pela nova imagem
+    header.classList.add("reduzido");
+    logo.src = "../images/NovaImagem.png"; // Substitua pela nova imagem
   } else {
-      header.classList.remove('reduzido');
-      logo.src = '../images/Logo 1000x1000.png'; // Volta para a imagem original
+    header.classList.remove("reduzido");
+    logo.src = "../images/Logo 1000x1000.png"; // Volta para a imagem original
   }
 });
